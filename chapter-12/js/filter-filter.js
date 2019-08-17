31ebb1cbd71d4b13a8f01c2b3fa5a8b2198ed201
Dragon8029 <dragon8029@gmail.com>
2019-08-17 08:33:18 -0700

    add filter-filter.js

---
+++ chapter-12/js/filter-filter.js
@@ -0,0 +1,41 @@
+$(function() {
+    var people = [
+        {
+            name: 'Casey',
+            rate: 60
+        },
+        {
+            name: 'Camille',
+            rate: 80
+        },
+        {
+            name: 'Gordon',
+            rate: 75
+        },
+        {
+            name: 'Nigel',
+            rate: 120
+        }
+    ];
+
+    // THE FUNCTION ACTS AS A FILTER
+    function priceRange(person) { // Declare priceRange()
+        return (person.rate >= 65) && (person.rate <= 90); // In range returns true
+    };
+    // FILTER THE PEOPLE ARRAY & ADD MATCHES TO THE RESULTS ARRAY
+    var results = []; // Array for matching people
+    results = people.filter(priceRange); // filter() calls priceRange()
+
+    // LOOP THROUGH NEW ARRAY AND ADD MATCHING PEOPLE TO THE RESULTS TABLE
+    var $tableBody = $('<tbody></tbody>'); // New content jQuery
+    for (var i = 0; i < results.length; i++) { // Loop through matches
+        var person = results[i]; // Store current person
+        var $row = $('<tr></tr>'); // Create a row for them
+        $row.append($('<td></td>').text(person.name)); // Add their name
+        $row.append($('<td></td>').text(person.rate)); // Add their rate
+        $tableBody.append( $row ) ; // Add row to new content
+    }
+
+    // Add the new content after the body of the page
+    $('thead').after($tableBody); // Add tbody after head
+});
